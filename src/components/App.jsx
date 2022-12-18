6780f3bad02d961419b5b686464b7ac3b4609c7f
Geljinskiy <savvageljinskiy@gmail.com>
2022-12-18 14:53:07 -0500

    add local strage saving

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,14 +11,26 @@ import css from './Common/Common.module.css';
 
 export class App extends React.Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    try {
+      const savedContacts = JSON.parse(localStorage.getItem('savedContacts'));
+      if (!savedContacts) {
+        return;
+      }
+      this.setState({ contacts: savedContacts });
+    } catch (error) {
+      console.log('error :', error);
+    }
+  }
+
+  componentDidUpdate() {
+    const { contacts } = this.state;
+    localStorage.setItem('savedContacts', JSON.stringify(contacts));
+  }
 
   onAddingContact = ({ name, number }) => {
     const isExist = this.state.contacts.filter(
